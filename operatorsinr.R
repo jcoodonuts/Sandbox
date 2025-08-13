339b96fe5624df17da451b4d77789e1b28faf6a8
jcoodonuts <06710067@dwc-legazpi.edu>
2025-08-13 16:52:16 +0800

    add operators

---
+++ operatorsinr.R
@@ -0,0 +1,24 @@
+# Assignment Operator
+var <- 42
+x = 10
+y = 3
+
+# Arithmetic Operators
+x + y
+x - y
+x * y
+x / y
+x ^ y
+x %% y
+
+# Comparison Operators
+x >= y
+x <= y
+x == y
+x != y
+
+# Association Operators
+
+x >= y & x != y
+x <= y | x == y
+
