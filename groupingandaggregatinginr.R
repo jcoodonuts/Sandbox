efa0efbe19e815283ddce4ff0e65d621aef9b960
jcoodonuts <06710067@dwc-legazpi.edu>
2025-08-22 20:43:42 +0800

    group by and aggregation

---
+++ groupingandaggregatinginr.R
@@ -0,0 +1,23 @@
+# Grouping and Aggregating in R
+library(dplyr)
+
+df <- read.csv("mcu_box_office_cleaned.csv")
+
+# Count the number of movies grouped by its specific mcu_phase value.
+df %>%
+  group_by(mcu_phase) %>%
+  summarize(movies = n())
+
+# Aggregate the grouped movies with mean, min, max, and median functions and set a column name for each aggregation.
+# mean() finds the average value in each group.
+# min() finds the lowest (minimum) value in each group. max() finds the highest (maximum) value in each group.
+# if group length n is even number, the median() is the average of the 2 values in the middle of the group.
+# if group length n is odd number, the median() is the value in the middle of the group.
+agg_df <- df %>%
+  group_by(mcu_phase) %>%
+  summarize(movies = n(),
+            avg_production_millions = mean(production_budget_millions),
+            lowest_budget_millions = min(production_budget_millions),
+            highest_budget_millions = max(production_budget_millions),
+            median_budget_millions = median(production_budget_millions))
+
